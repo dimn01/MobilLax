0c7601a3e9d0f20f26d95c201c7f57246d50e931
Abbbs2ws <kyb3348@naver.com>
2025-04-01 16:02:18 +0900

    First Spring boot Setting

---
+++ src/test/java/MobilLax/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package MobilLax.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
