ab2a440866af986c11d280aa6e299737517c4049
DongJu <zz561199@gmail.com>
2025-04-12 21:34:04 +0900

    carRoute & backup

--- out/production/resources/static/module/script.js
+++ out/production/resources/static/module/script.js
@@ -1,4 +1,6 @@
-function showSuggestions(value, type) {
+document.write('<script scr="/init.js"></script>');
+
+async function showSuggestions(value, type) {
   const suggestions = type === 'from' ? document.getElementById("fromSuggestions") : document.getElementById("toSuggestions");
 
   if (!value.trim()) {
@@ -6,28 +8,63 @@ function showSuggestions(value, type) {
     return;
   }
 
-  const ps = new kakao.maps.services.Places();
-  ps.keywordSearch(value, function (data, status) {
-    if (status !== kakao.maps.services.Status.OK) {
-      suggestions.innerHTML = '<div>검색 결과가 없습니다.</div>';
-      return;
-    }
+  const headers = {
+    "appKey": "gyeuBycm5d6tSeRpNvNyq1dm6YctkXF29812OT8D"
+  };
 
-    const html = data.slice(0, 5).map(place => `
-      <div onclick="selectSuggestion('${place.place_name}', '${type}')">
-        <i class='fas fa-map-marker-alt'></i>
-        <span>${place.place_name}</span>
-        <small style='margin-left:auto;'>${place.address_name}</small>
-      </div>
-    `).join('');
-
-    suggestions.innerHTML = html;
+  const params = new URLSearchParams({
+    "version": 2,
+    "searchKeyword": value,
+    "resCoordType": "EPSG3857",
+    "reqCoordType": "WGS84GEO",
+    "searchtypCd": "A",
+//    "centerLon": n_lng,
+//    "centerLat": n_lat,
+//    "radius": "0",
+    "count": 5
   });
+
+  try {
+    const response = await fetch(`https://apis.openapi.sk.com/tmap/pois?version=1&format=json&callback=result&${params.toString()}`, {
+    method: "GET",
+    headers: headers
+  });
+
+    if(response.status != 200) {
+        suggestions.innerHTML = '<div>검색 결과가 없습니다.</div>';
+        return;
+    }
+    const data = await response.json();
+    const resultpoisData = data.searchPoiInfo.pois.poi;
+
+    let suggest = "";
+
+    for (let k in resultpoisData) {
+        const poi = resultpoisData[k];
+        const name = poi.name;
+        const addrName = poi.upperAddrName + " " + poi.middleAddrName
+            + " " + poi.lowerAddrName;
+        const a_lat = poi.noorLat;
+        const a_lon = poi.noorLon;
+
+        suggest += `<div onclick='selectSuggestion("${name}", "${type}", "${a_lat}", "${a_lon}")'>
+            <i class='fas fa-map-marker-alt'></i>
+            <span>${name}</span>
+            <small style='margin-left:auto;'>${addrName}</small>
+        </div>`;
+    }
+    suggestions.innerHTML = suggest;
+  } catch (error) {
+    console.error("API 호출 중 오류 발생:", error);
+  }
 }
 
-function selectSuggestion(place, type) {
+function selectSuggestion(place, type, lat, lon) {
   document.getElementById(type + "Input").value = place;
   document.getElementById(type + "Suggestions").innerHTML = '';
+
+  if (type == 'from') { setStart(lat, lon); }
+  if (type == 'to') { setEnd(lat, lon); }
 }
 
 function swapInputs() {
--- src/main/resources/static/module/init.js
+++ src/main/resources/static/module/init.js
@@ -1,6 +1,6 @@
-var map, lat, lng, now, marker, s_marker, e_marker;
+var map, lat, lng, now, marker;
 var n_lat, n_lng;
-var markers = [];
+var markers = [], arr = []; //시작 도착 마커 배열
 
 function initTmap() {
         map = new Tmapv2.Map("map_div", {
@@ -11,38 +11,38 @@ function initTmap() {
     });
 
     // 현위치 마커
-    if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-        function(position) {
-            n_lat = position.coords.latitude;
-            n_lng = position.coords.longitude;
-
-            // 팝업 생성
-            // let content = "<div style=' position: relative; border-bottom: 1px solid #dcdcdc; line-height: 18px; padding: 0 3px 2px 0;'>"
-            // 		+ "<div style='font-size: 12px; line-height: 15px;'>"
-            // 		+ "<span style='display: inline-block; width: 4px; height: 5px; background-image: url(/resources/images/common/icon_blet.png); vertical-align: middle; margin-right: 5px;'></span>현재위치"
-            // 		+ "</div>" + "</div>";
-
-            // 현위치 마커 생성
-            // let marker = new Tmapv2.Marker({
-            // 	position : new Tmapv2.LatLng(n_lat,n_lng),
-            // 	map : map
-            // });
-
-            // 마커 설명
-            // InfoWindow = new Tmapv2.InfoWindow({
-            // 	position : new Tmapv2.LatLng(n_lat,n_lng),
-            // 	content : content,
-            // 	type : 2,
-            // 	map : map
-            // });
-
-            // 현위치
-            now = new Tmapv2.LatLng(n_lat,n_lng);
-            map.setCenter(now);
-            map.setZoom(15);
-        });
-    }
+//    if (navigator.geolocation) {
+//    navigator.geolocation.getCurrentPosition(
+//        function(position) {
+//            n_lat = position.coords.latitude;
+//            n_lng = position.coords.longitude;
+//
+//            // 팝업 생성
+//            // let content = "<div style=' position: relative; border-bottom: 1px solid #dcdcdc; line-height: 18px; padding: 0 3px 2px 0;'>"
+//            // 		+ "<div style='font-size: 12px; line-height: 15px;'>"
+//            // 		+ "<span style='display: inline-block; width: 4px; height: 5px; background-image: url(/resources/images/common/icon_blet.png); vertical-align: middle; margin-right: 5px;'></span>현재위치"
+//            // 		+ "</div>" + "</div>";
+//
+//            // 현위치 마커 생성
+//            // let marker = new Tmapv2.Marker({
+//            // 	position : new Tmapv2.LatLng(n_lat,n_lng),
+//            // 	map : map
+//            // });
+//
+//            // 마커 설명
+//            // InfoWindow = new Tmapv2.InfoWindow({
+//            // 	position : new Tmapv2.LatLng(n_lat,n_lng),
+//            // 	content : content,
+//            // 	type : 2,
+//            // 	map : map
+//            // });
+//
+//            // 현위치
+//            now = new Tmapv2.LatLng(n_lat,n_lng);
+//            map.setCenter(now);
+//            map.setZoom(15);
+//        });
+//    }
 
     map.addListener("contextmenu", onContextmenu);
     map.addListener("click", onClick);
@@ -66,6 +66,31 @@ function removeMarker() {
     marker.setMap(null);
     marker = null;
 }
+// Marker 생성 | color(b,g,r,w) size(b,m,s) name(0~9/a~z)
+// function setMarker(i_lat, i_lon, c, s, n)
+function setMarker(infoObj) {
+    if(!map) {alert('no'); return;}
+
+    let i_marker = new Tmapv2.Marker({
+        position : new Tmapv2.LatLng(infoObj.lat, infoObj.lng),
+        icon : infoObj.markerImage,
+        map : map
+    });
+    if(infoObj.pointType == 'start') {
+        arr[0] = i_marker;
+        resultMarkerArr.push(i_marker);
+    }
+    else if(infoObj.pointType == 'end') {
+        arr[1] = i_marker;
+        resultMarkerArr.push(i_marker);
+    }
+    else if(infoObj.poingType == 'P') {
+        // 포인터 마커 안나오게 설정
+        i_marker.setMap(null);
+        resultMarkerArr.push(i_marker);
+    }
+
+}
 
 // 좌클릭
 function onClick(e) {
@@ -85,7 +110,6 @@ function onContextmenu(e) {
         map: map
     });
     markers.push(marker);
-
     // var lngLat = e.lngLat;
     // console.log(lngLat);
 
@@ -99,19 +123,26 @@ function markerClick(e) {
     alert('wow');
 }
 
-function setStart() {
-    removeMarker();
-    if (s_marker != null) {
-        s_marker.setMap(null);
-    }
-    s_marker = new Tmapv2.Marker({
-        position: new Tmapv2.LatLng(lat,lng),
-        icon: `https://tmapapi.tmapmobility.com/upload/tmap/marker/pin_b_m_s.png`,
-        title: 'start',
-        map: map
-    });
+function setStart(s_lat, s_lng) {
+    if (markers.length > 0) { removeMarker(); }
+    if (arr[0]) { arr[0].setMap(null); arr[0] = null; }
+
+    if (s_lat == 0 && s_lng == 0) { s_lat = lat; s_lng = lng;}
+    var infoObj = {
+        markerImage : 'https://tmapapi.tmapmobility.com/upload/tmap/marker/pin_b_m_s.png',
+        lng : s_lng,
+        lat : s_lat,
+        pointType : 'start'
+    };
+    setMarker(infoObj);
+//    setMarker(s_lat, s_lng, 'b', 'm', 's');
+//    s_marker = new Tmapv2.Marker({
+//        position: new Tmapv2.LatLng(s_lat,s_lng),
+//        icon: `https://tmapapi.tmapmobility.com/upload/tmap/marker/pin_b_m_s.png`,
+//        title: 'start',
+//        //map: map
+//    });
     hideContextMenu();
-    console.log(s_marker.getPosition());
 }
 
 function setWaypoint() {
@@ -119,17 +150,25 @@ function setWaypoint() {
     hideContextMenu();
 }
 
-function setEnd() {
-    removeMarker();
-    if (e_marker != null) {
-        e_marker.setMap(null);
-    }
-    e_marker = new Tmapv2.Marker({
-        position: new Tmapv2.LatLng(lat,lng),
-        icon: `https://tmapapi.tmapmobility.com/upload/tmap/marker/pin_r_m_e.png`,
-        title: 'end',
-        map: map
-    });
+function setEnd(e_lat, e_lng) {
+    if (marker) { removeMarker(); }
+    if (arr[1]) { arr[1].setMap(null); arr[1] = null; }
+
+    if (e_lat == 0 && e_lng == 0) { e_lat = lat; e_lng = lng;}
+    var infoObj = {
+        markerImage : 'https://tmapapi.tmapmobility.com/upload/tmap/marker/pin_r_m_e.png',
+        lng : e_lng,
+        lat : e_lat,
+        pointType : 'end'
+    };
+    setMarker(infoObj);
+//    setMarker(e_lat, e_lng, 'r', 'm', 'e');
+//    e_marker = new Tmapv2.Marker({
+//        position: new Tmapv2.LatLng(lat,lng),
+//        icon: `https://tmapapi.tmapmobility.com/upload/tmap/marker/pin_r_m_e.png`,
+//        title: 'end',
+//        //map: map
+//    });
     hideContextMenu();
 }
 
