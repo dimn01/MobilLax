192ea78681be95425589ea8e33531e825d6caa9f
Abbbs2ws <kyb3348@naver.com>
2025-04-19 02:20:50 +0900

    Revert "add DB SignUp"
    This reverts commit c6e8ace6b3110575e20250389dde450fcedb4f33.

--- src/main/java/MobilLax/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package MobilLax.Repository;
-
-import MobilLax.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-}
