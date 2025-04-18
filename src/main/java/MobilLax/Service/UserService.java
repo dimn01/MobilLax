3c67460626c37c2834ba196ffad3d7770a9f731e
Abbbs2ws <kyb3348@naver.com>
2025-04-19 02:18:22 +0900

    Revert "add BCryptPassword"
    This reverts commit f2b1747904234c3e1720cc3ebd01c16539de40a8.

--- src/main/java/MobilLax/Service/UserService.java
+++ src/main/java/MobilLax/Service/UserService.java
@@ -3,29 +3,17 @@ package MobilLax.Service;
 import MobilLax.Model.User;
 import MobilLax.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
-    private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
-
     @Autowired
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
+    private UserRepository userRepository;
 
     public boolean registerUser(User user) {
         if (userRepository.existsByEmail(user.getEmail())) {
             return false; // 이미 이메일이 존재함
         }
-        // 비밀번호 암호화
-        String encryptedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encryptedPassword);
         userRepository.save(user);
         return true;
     }
